a6127dd4c1ed2bd46f825d1f7aea8e0d9a65e4f2
crisp-convention-4859 <slluong@uw.edu>
2019-07-04 08:36:41 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,27 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array = array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  array = array.pop(2)
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array)
+  array = array.shift(2)
+end
+
+def using_concat(array1, array2)
+  array = using_concat(array1, array2)
+end
