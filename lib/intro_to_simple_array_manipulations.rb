14e554dcda70f1aa81e3fdd2d47d9ba5501a80f0
crisp-convention-4859 <slluong@uw.edu>
2019-07-04 08:42:20 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -34,3 +34,16 @@ def using_uniq(array)
   array = array.uniq 
 end
 
+def using_flatten(array)
+  array = array.flatten 
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, integer)
+  array = array.delete(integer)
+end
+
+
