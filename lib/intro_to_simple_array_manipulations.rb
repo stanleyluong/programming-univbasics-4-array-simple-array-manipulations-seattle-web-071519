75d1bc14f5002f7e85cde18e64f6e70a444ed5cd
crisp-convention-4859 <slluong@uw.edu>
2019-07-04 09:12:20 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -43,7 +43,13 @@ def using_delete(array, string)
 end
 
 def using_delete_at(array, integer)
-  integer = array.delete(integer)
+  array = array.delete(integer)
 end
 
-
+describe "using_delete_at" do 
+  it "takes in two arguments, an array and an integer and deletes the element at the index of the array that is equal to that integer" do 
+    famous_robots = ["Johnny 5", "R2D2", "Robocop"]
+    deleted_robot = using_delete_at(famous_robots, 2)
+    expect(deleted_robot).to eq("Robocop")
+  end
+end
